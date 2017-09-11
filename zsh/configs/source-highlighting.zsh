e9c1630a83f7369343174456eb80e3c6f2e06953
Brad Urani <bradurani@gmail.com>
2017-09-11 03:02:56 -0700

    Add less pipe src highlight for brew path

--- zsh/configs/source-highlighting.zsh
+++ zsh/configs/source-highlighting.zsh
@@ -4,3 +4,9 @@ if [ -s '/usr/share/source-highlight/src-hilite-lesspipe.sh' ]; then
   export LESSOPEN="| /usr/share/source-highlight/src-hilite-lesspipe.sh %s"
   export LESS=" -R "
 fi
+
+if [ -s '/usr/local/bin/src-hilite-lesspipe.sh' ]; then
+  export LESSOPEN="| /usr/local/bin/src-hilite-lesspipe.sh %s"
+  export LESS=" -R "
+fi
+
