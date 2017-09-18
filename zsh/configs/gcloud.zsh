490af238f71cef9a355e4663bd9041da83f658fd
Brad Urani <bradurani@gmail.com>
2017-09-18 01:47:32 -0500

    Add gcloud config

---
+++ zsh/configs/gcloud.zsh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+if [ -f "$HOME/google-cloud-sdk/completion.zsh.inc" ]; then
+  source "$HOME/google-cloud-sdk/completion.zsh.inc"
+fi
+
+if [ -f "$HOME/google-cloud-sdk/path.zsh.inc" ]; then
+  source "$HOME/google-cloud-sdk/path.zsh.inc"
+fi
